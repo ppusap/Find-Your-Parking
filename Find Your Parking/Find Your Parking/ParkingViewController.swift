72c3edddb0180728919037376a00cf4a011738d2
Pratyusha Pusapati <pratyushavarma.p@gmail.com>
2019-10-08 18:54:11 -0500

    updated parking details view controller

--- Find Your Parking/Find Your Parking/ParkingViewController.swift
+++ Find Your Parking/Find Your Parking/ParkingViewController.swift
@@ -2,29 +2,38 @@
 //  ParkingViewController.swift
 //  Find Your Parking
 //
-//  Created by student on 10/8/19.
+//  Created by Student on 10/5/19.
 //  Copyright © 2019 Bearcat Coders. All rights reserved.
 //
 
 import UIKit
 
 class ParkingViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+        
     }
     
-
+    
+    
+    
+    
+    
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    
 }

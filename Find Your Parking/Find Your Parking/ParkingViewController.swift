890d7beafceb12724fdfdb059c6548908469685f
Pratyusha Pusapati <pratyushavarma.p@gmail.com>
2019-10-08 18:35:12 -0500

    added Parking view controller files

---
+++ Find Your Parking/Find Your Parking/ParkingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ParkingViewController.swift
+//  Find Your Parking
+//
+//  Created by student on 10/8/19.
+//  Copyright © 2019 Bearcat Coders. All rights reserved.
+//
+
+import UIKit
+
+class ParkingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

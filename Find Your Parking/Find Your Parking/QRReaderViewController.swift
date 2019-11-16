3346942229fcec11dac5461f7fd187051c4dabb4
Student <student@nw46151.local>
2019-11-15 19:49:17 -0600

    Felipe Sato QR Scanner Added

--- Find Your Parking/Find Your Parking/QRReaderViewController.swift
+++ Find Your Parking/Find Your Parking/QRReaderViewController.swift
@@ -5,84 +5,62 @@
 //  Created by Student on 10/4/19.
 //  Copyright © 2019 Bearcat Coders. All rights reserved.
 //
-/*
+
 import UIKit
 import AVFoundation
 
 class QRReaderViewController: UIViewController {
-
-    var session = AVCaptureSession()
-    var frontCamera: AVCaptureDevice?
-    var backCamera: AVCaptureDevice?
-    var currentCamera: AVCaptureDevice?
-    var photoOutput: AVCapturePhotoOutput?
-    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
-    
+    var video = AVCaptureVideoPreviewLayer()
+    @IBOutlet weak var aimIV: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCaputerSession()
-        setupDevice()
-        setupInputOutput()
-        setupPreviewLayer()
-        startRunningCaputerSession()
-    }
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        self.tabBarItem.title = "QR Scanner"
-        tabBarItem.image = UIImage(named: "QR")
+        // Do any additional setup after loading the view, typically from a nib.
+        let session = AVCaptureSession()
+        
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+        }
+        catch{
+            print("Error")
+        }
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self as? AVCaptureMetadataOutputObjectsDelegate, queue: DispatchQueue.main)
+        
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: aimIV)
+        
+        session.startRunning()
         
     }
     
-    @IBAction func cameraButton(_ sender: Any) {
-        performSegue(withIdentifier: "Photo", sender: nil)
+    func captureOutput(_ captureOutput: AVCaptureMetadataOutput, didOutputMetadataObject metadataObjects: [Any]!, from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObject.ObjectType.qr {
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: {(nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    func setupCaputerSession(){
-        session.sessionPreset = AVCaptureSession.Preset.photo
-    }
-    
-    func setupDevice(){
-        let device = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
-        
-        let devices = device.devices
-        for usingDevice in devices {
-            if usingDevice.position == AVCaptureDevice.Position.back {
-                backCamera = usingDevice
-            }
-            else if usingDevice.position == AVCaptureDevice.Position.front {
-                frontCamera = usingDevice
-            }
-        }
-        
-        currentCamera = backCamera
-    }
-    
-    func setupInputOutput(){
-        do {
-            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
-            session.addInput(captureDeviceInput)
-            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
-        }
-        catch{
-            print(error)
-        }
-        
-        
-    }
-    
-    func setupPreviewLayer() {
-        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
-        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
-        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
-        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
-    }
-    
-    func startRunningCaputerSession() {
-        session.startRunning()
-    }
- }/**/*/
+ }

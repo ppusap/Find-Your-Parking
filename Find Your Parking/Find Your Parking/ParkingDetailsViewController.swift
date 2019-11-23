a20edb14ea3d008cecf69194111d3b494675fe24
Rishika Reddy Gaddam <rishika.reddy.g5@gmail.com>
2019-11-22 19:33:33 -0600

    Made changes in QRReaderViewController and ParkingDetailsViewController

---
+++ Find Your Parking/Find Your Parking/ParkingDetailsViewController.swift
@@ -0,0 +1,123 @@
+//
+//  SecondViewController.swift
+//  Find Your Parking
+//
+//  Created by Student on 10/4/19.
+//  Copyright © 2019 Bearcat Coders. All rights reserved.
+//
+
+import UIKit
+
+class ParkingDetailsViewController: UIViewController {
+    
+    var scanData:String?
+    @IBOutlet weak var superMarketNameLBL:UILabel!
+    @IBOutlet weak var parkingLotLocationLBL:UILabel!
+    @IBOutlet weak var parkingNumberLBL: UILabel!
+    
+    @IBAction func viewMap(_ sender: Any) {
+        print("Button pressed")
+        self.performSegue(withIdentifier:"ParkingViewSegue", sender: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.navigationController?.tabBarItem.title = "Where's my car?"
+        self.navigationController?.tabBarItem.image = UIImage(named: "Map")
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        scanData = Supermarkets.shared.scanStorageData
+        
+        guard let displayData = scanData?.split(separator: "-") else{
+            return
+        }
+        
+        if String(displayData[0]) == "w" || String(displayData[0]) == "W"{
+            superMarketNameLBL.text = "Walmart"
+            if String(displayData[1]) == "s" || String(displayData[1]) == "S"{
+                parkingLotLocationLBL.text = "South"
+
+            }else if String(displayData[1]) == "n" || String(displayData[1]) == "N"{
+                parkingLotLocationLBL.text = "North"
+
+            }
+            else if String(displayData[1]) == "m" || String(displayData[1]) == "M"{
+                parkingLotLocationLBL.text = "Main"
+                
+                
+            }
+            else if String(displayData[1]) == "e" || String(displayData[1]) == "E"{
+                parkingLotLocationLBL.text = "East"
+    
+                
+            }
+            parkingNumberLBL.text = scanData
+        }else if String(displayData[0]) == "h" || String(displayData[0]) == "H"{
+            superMarketNameLBL.text = "Hyvee"
+            if String(displayData[1]) == "s" || String(displayData[1]) == "S"{
+                parkingLotLocationLBL.text = "South"
+  
+                
+            }else if String(displayData[1]) == "n" || String(displayData[1]) == "N"{
+                parkingLotLocationLBL.text = "North"
+ 
+            }
+            else if String(displayData[1]) == "m" || String(displayData[1]) == "M"{
+                parkingLotLocationLBL.text = "Main"
+
+                
+            }
+            else if String(displayData[1]) == "e" || String(displayData[1]) == "E"{
+                parkingLotLocationLBL.text = "East"
+ 
+                
+            }
+            parkingNumberLBL.text = scanData
+        }else if String(displayData[0]) == "d" || String(displayData[0]) == "D"{
+            superMarketNameLBL.text = "Dollar General"
+            if String(displayData[1]) == "s" || String(displayData[1]) == "S"{
+                parkingLotLocationLBL.text = "South"
+ 
+                
+            }else if String(displayData[1]) == "n" || String(displayData[1]) == "N"{
+                parkingLotLocationLBL.text = "North"
+
+                
+            }
+            else if String(displayData[1]) == "m" || String(displayData[1]) == "M"{
+                parkingLotLocationLBL.text = "Main"
+
+            }
+            else if String(displayData[1]) == "e" || String(displayData[1]) == "E"{
+                parkingLotLocationLBL.text = "East"
+
+                
+            }
+            parkingNumberLBL.text = scanData
+        }
+        
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //        self.navigationController?.tabBarItem.title = "Map"
+        //        self.navigationController?.tabBarItem.image = UIImage(named: "Map")
+        navigationItem.title = "Details of Parking"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @objc func cancel(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Find Your Parking/Find Your Parking/QRReaderViewController.swift
+++ Find Your Parking/Find Your Parking/QRReaderViewController.swift
@@ -9,58 +9,195 @@
 import UIKit
 import AVFoundation
 
-class QRReaderViewController: UIViewController {
-    var video = AVCaptureVideoPreviewLayer()
-    @IBOutlet weak var aimIV: UIImageView!
+class QRReaderViewController: UIViewController{
+
+    var slot:Slot!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let session = AVCaptureSession()
-        
-        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice!)
-            session.addInput(input)
-        }
-        catch{
-            print("Error")
-        }
-        let output = AVCaptureMetadataOutput()
-        session.addOutput(output)
-        
-        output.setMetadataObjectsDelegate(self as? AVCaptureMetadataOutputObjectsDelegate, queue: DispatchQueue.main)
-        
-        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        
-        video = AVCaptureVideoPreviewLayer(session: session)
-        video.frame = view.layer.bounds
-        view.layer.addSublayer(video)
-        
-        self.view.bringSubview(toFront: aimIV)
-        
-        session.startRunning()
+    
+    var captureSession = AVCaptureSession()
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    //@IBOutlet weak var aimIV: UIImageView!
+
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        tabBarItem.title = "QR Code"
+        tabBarItem.image = UIImage(named: "QR")
         
     }
     
-    func captureOutput(_ captureOutput: AVCaptureMetadataOutput, didOutputMetadataObject metadataObjects: [Any]!, from connection: AVCaptureConnection) {
-        if metadataObjects != nil && metadataObjects.count != 0 {
-            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
-                if object.type == AVMetadataObject.ObjectType.qr {
-                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
-                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: {(nil) in
-                        UIPasteboard.general.string = object.stringValue
-                    }))
-                    
-                    present(alert, animated: true, completion: nil)
-                }
-            }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Get the back-facing camera for capturing videos
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            print("Failed to get the camera device")
+            return
+        }
+        
+
+        
+        do {
+            // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Set the input device on the capture session.
+            captureSession.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self as! AVCaptureMetadataOutputObjectsDelegate, queue: DispatchQueue.main)
+            // captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+        } catch {
+            // If any error occurs, simply print it out and don't continue any more.
+            print(error)
+            return
+        }
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // Start video capture.
+        captureSession.startRunning()
+        
+        // Move the message label and top bar to the front
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: topbar)
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
- }
+    
+    // MARK: - Helper methods
+    func launchApp(decodedURL: String) {
+        
+        if presentedViewController != nil {
+            return
+        }
+        Supermarkets.shared.scanStorageData=decodedURL
+        print("The data is:"+Supermarkets.shared.scanStorageData)
+        let alertPrompt = UIAlertController(title: "Open App", message: "\(decodedURL)", preferredStyle: .actionSheet)
+        let confirmAction = UIAlertAction(title: "Confirm", style: UIAlertAction.Style.default, handler: { (action) -> Void in
+            
+            
+            let mapViewID = self.storyboard?.instantiateViewController(withIdentifier: "MapViewVCID") as! UINavigationController
+            let navConValue = mapViewID.topViewController as? ParkingDetailsViewController
+            self.present(mapViewID, animated: true, completion: nil)
+            navConValue!.scanData = decodedURL
+        })
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+            
+            alertPrompt.addAction(confirmAction)
+            alertPrompt.addAction(cancelAction)
+            
+            present(alertPrompt, animated: true, completion: nil)
+            
+//        }
+//
+//
+//        else
+//        {
+//            print("Sorry scanned URL is not correct")
+//            let ac = UIAlertController(title: "Oops you can't park here!!", message: "Invalid QR Code", preferredStyle: .alert)
+//            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+//            ac.addAction(action)
+//            self.present(ac, animated: true)
+//        }
+        
+      
+       
+        
+    }
+    private func updatePreviewLayer(layer: AVCaptureConnection, orientation: AVCaptureVideoOrientation) {
+        layer.videoOrientation = orientation
+        videoPreviewLayer?.frame = self.view.bounds
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let connection =  self.videoPreviewLayer?.connection  {
+            let currentDevice: UIDevice = UIDevice.current
+            let orientation: UIDeviceOrientation = currentDevice.orientation
+            let previewLayerConnection : AVCaptureConnection = connection
+            
+            if previewLayerConnection.isVideoOrientationSupported {
+                switch (orientation) {
+                case .portrait:
+                    updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+                    break
+                case .landscapeRight:
+                    updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeLeft)
+                    break
+                case .landscapeLeft:
+                    updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeRight)
+                    break
+                case .portraitUpsideDown:
+                    updatePreviewLayer(layer: previewLayerConnection, orientation: .portraitUpsideDown)
+                    break
+                default:
+                    updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+                    break
+                }
+            }
+        }
+    }
+    
+}
+
+extension QRReaderViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if  metadataObj.type == AVMetadataObject.ObjectType.qr{
+            // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                launchApp(decodedURL: metadataObj.stringValue!)
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+
+
+    
+    
+    
+    
+}
